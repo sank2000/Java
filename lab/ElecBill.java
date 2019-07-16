aab5a3b24df9678bff69f32eefa8e0d323ed923d
Santhosh <51191409+sank2000@users.noreply.github.com>
2019-07-16 12:18:24 +0530

    v2.0

--- lab/ElecBill.java
+++ lab/ElecBill.java
@@ -1,6 +1,8 @@
+package lab;
 import java.util.Scanner;
 class Bill{
-    int custid,preread,curread,type,read;
+    int custid,type;
+    double preread,curread,read;
     double rs;
     String custname;
     void getdata(){
@@ -10,11 +12,13 @@ class Bill{
         System.out.print("Enter customer name:");
         custname=scan.next();
         System.out.print("Enter the previous month reading:");
-        preread=scan.nextInt();
+        preread=scan.nextDouble();
         System.out.print("Enter the current month reading:");
-        curread=scan.nextInt();
+        curread=scan.nextDouble();
     }
     void calc(){
+    	if(preread>curread)
+    		return;
      read=curread-preread;
      if(type==0)
      {
@@ -43,10 +47,16 @@ class Bill{
      }
     void display(){
         System.out.println("\n\n\n---------ELECTRICITY BILL--------\n" );
-         System.out.println("Customer id:"+custid);
-         System.out.println("\nCustomer name: " + custname);
-         System.out.println("\nReading:"+read);
-         System.out.printf("\nAmount = %.2f ",rs);    
+         System.out.println("Customer id        :"+custid);
+         System.out.println("\nCustomer name      :"+custname);
+         if(type==0)
+        	 System.out.println("\nCustomer type      :Domestic");
+         else
+        	 System.out.println("\nCustomer type      :Commercial");
+         System.out.println("\nPrevious Reading   :"+preread);
+         System.out.println("\nCurrent Reading    :"+curread);
+         System.out.println("\nUnits Consumed     :"+read);
+         System.out.printf("\nAmount             :Rs.%.2f",rs);    
     } 
 }
 public class ElecBill
@@ -62,9 +72,17 @@ public class ElecBill
         for(i=0;i<num;i++)
         {
             obj[i]=new Bill();
+            boolean j = false;
+            do
+            {
+            	if(j)
+            		System.out.printf("\nYou are allowed only to enter 0 or 1 !\n");
+            		
             System.out.printf("\t\tTYPE\n\t0.Domestic\n\t1.Commercial ");
             System.out.printf("\nSelect your type(0/1):");
             obj[i].type=scan.nextInt();
+            j=true;
+            }while(obj[i].type != 0 && obj[i].type != 1);
             obj[i].getdata();
             obj[i].calc();
         }
